a319aa194085e53f8e2a6b2ead3e3ca7e43abd7f
Chenthill Palanisamy <chenthillrulz@gmail.com>
2016-12-05 16:27:07 +0530

    update 2_3_4 tree implementation

--- 2_3_4_tree.py
+++ 2_3_4_tree.py
@@ -46,3 +46,36 @@ class Node (self):
 
 
         return True
+
+fo = open("/home/pchenthill/practice/bst.txt", "w")
+fo.write("@startuml\n")
+fo.write("digraph 2_3_4_tree {\n")
+fo.write("node [shape = record,height=.1];")
+
+node_count = 0
+
+def make_graph (node, parent_label):
+    if node == None:
+        return
+    global node_count
+
+    cur_node_name = "node" + str(node_count)
+    fo.write(cur_node_name + "[label = \"")
+    string = "<f0> "
+    i = 1
+    for key in node.keys:
+        string += "|" +  str(key) +  "|" + "<f" + str (i) + ">"
+        i = i + 1
+    string += "];"
+
+    if node.parent != None:
+        conn = "\"" + parent_label + "->" + cur_node_name
+
+    node_count += 1
+    for i, child in enumerate(node.children)
+        make_graph (child, "\"" + cur_node_name +  "\":f" + str(i))
+
+make_graph (root)
+fo.write("}\n")
+
+fo.close()
